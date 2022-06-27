92560f87f08d5c4d83b6c7c1c72a59afef9c53cd
Carlos Freitas <carlos.gabriel@aluno.ufop.edu.br>
2022-06-27 16:43:15 -0300

    Lançamento - Alpha v0.0.3

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import random
 from colorama import init
 
-from classes_base.criatura import *
-from classes_base.item import *
-from classes_base.utils import * ##
 from menus.inicial import *
 
 # Lembrete: Quando uma nova arma for criada e ela for de um tipo que não é "Normal", ela deve alterar o tipo
 # do ataque normal do jogador em EquipadosGanhos()
-
-# Lembrete: Quando um item equipável aumenta maxHp/maxMana, ele também deve aumentar hp/mana
 
 # .py -> .exe : pyinstaller --onefile main.py
 
@@ -17,12 +12,8 @@ from menus.inicial import *
 # BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE
 # Fore. ; Back.
 
-# Inicialização do colorama
-random.seed()
-init()
-
-# Printando uma frase com delay nas letras:
-# ImprimirComDelay("isso aqui é um teste, lol!\n", 0.04)
-############################################
+# Inicialização
+random.seed() # Inicialização de uma semente aleatória
+init()        # Inicialização da biblioteca colorama
 
 MenuInicial()
